adc493adf34687780716d2f3443f950aa773144b
yousef harara <hrarty37@gmail.com>
2026-05-11 17:38:25 +0300

    - start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-red-400 p-4'>Hello from App</h1>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='bg-green-500 text-4xl text-white font-bold p-4'> 
+            Hi, Yousef 
+        </div>
+    );
+}
+
+export default HomePage;
