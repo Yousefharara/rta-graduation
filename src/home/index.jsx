40af49b58157466022ff3276b12d8c4a46bdc81d
yousef harara <hrarty37@gmail.com>
2026-05-11 17:59:35 +0300

    - adding dependencies

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div className='bg-green-500 text-4xl text-white font-bold p-4'> 
+        <div className='bg-green-500 shadow-xl text-shadow text-4xl text-white font-bold p-4'> 
             Hi, Yousef 
         </div>
     );
